0549a42948ddf9545f73ba2f5fdf977fc5baf9e8
Dennis DeVries <7dennisdv7@gmail.com>
2022-06-28 13:27:12 -0400

    adding the about feature

--- src/About.js
+++ src/About.js
@@ -1,10 +1,22 @@
 import { Component } from "react";
+import Card from 'react-bootstrap/Card';
 
 class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+        <Card>
+          <Card.Text>Dennis DeVries</Card.Text>
+          <Card.Text>Army vet excited to enter the tech industry!</Card.Text>
+        </Card>
+        <Card>
+          <Card.Text>Kris Dunning</Card.Text>
+          <Card.Text>Air Force vet entering the tech industry as a software developer</Card.Text>
+        </Card>
+      </>
+    )      
   }
 };
 
