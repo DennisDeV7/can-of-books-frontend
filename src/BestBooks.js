4e154dc516bf166b64ded7f7db5eb094a493a91f
Dennis DeVries <7dennisdv7@gmail.com>
2022-06-28 20:10:50 -0400

    adding form and create data function

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import Bookshelf from './Bookshelf';
+import BookModal from './BookFormModal';
+import Button from 'react-bootstrap/Button';
 
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      show: false
     }
   }
 
@@ -23,6 +26,44 @@ class BestBooks extends React.Component {
       console.log('error: ', error.response.data)
     }
   }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Handler fired');
+    let newBook = {
+      title: e.target.newBookTitle.value,
+      description: e.target.newBookDescription.value,
+      status: e.target.newBookStatus.value
+    }
+    console.log(newBook);
+    this.postBooks(newBook)
+  }
+
+  postBooks = async (newBookObj) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`;
+      let createdBook = await axios.post(url, newBookObj);
+      console.log(createdBook);
+      this.setState({
+        books: [...this.state.books, createdBook.data]
+      });
+    } catch (error) {
+      console.log('error:', error.response.data);
+    }
+  }
+
+
+  handleOpen = () => {
+    this.setState({
+      show: true
+    })
+  }
+
+  handleonHide = () => {
+    this.setState({
+      show: false
+    });
+  };
 
   componentDidMount() {
     this.getBooks();
@@ -43,6 +84,14 @@ class BestBooks extends React.Component {
         ) : (
           <h3>Bookshelf is currently empty</h3>
         )}
+        <BookModal
+          show={this.state.show}
+          hideModal={this.handleonHide}
+          newBookData={this.handleFormSubmit}
+          />
+        <Button onClick={this.handleOpen}>
+          Add New Book
+        </Button>
       </>
     )
   }
